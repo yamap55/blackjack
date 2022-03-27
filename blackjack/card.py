d86102df94b0944a51b58a1f414513fa43d94855
yamap55 <1785817+yamap55@users.noreply.github.com>
2022-03-27 23:36:32 +0900

    [refactor] convert dataclass

--- blackjack/card.py
+++ blackjack/card.py
@@ -1,18 +1,19 @@
 """
 card
 """
+from dataclasses import dataclass
+
 from blackjack.suit import Suit
 
 
+@dataclass(frozen=True)
 class Card:
     """
     Card
     """
 
-    def __init__(self, suit: Suit, number: int) -> None:
-        """init"""
-        self.suit = suit
-        self.number = number
+    suit: Suit
+    number: int
 
     def __str__(self) -> str:
         """str"""
@@ -22,8 +23,32 @@ class Card:
         """repr"""
         return str({"suit": self.suit.name, "number": self.number})
 
-    def __eq__(self, __o: object) -> bool:
-        """equal"""
-        if not isinstance(__o, Card):
+    def __lt__(self, other: object) -> bool:
+        """lt"""
+        if not isinstance(other, Card):
+            return NotImplemented
+        if self.suit != other.suit:
+            return self.suit.value < other.suit.value
+        return self.number < other.number
+
+    def __eq__(self, other) -> bool:
+        """eq"""
+        if not isinstance(other, Card):
             return False
-        return self.suit == __o.suit and self.number == __o.number
+        return super().__eq__(other)
+
+    def __ne__(self, other) -> bool:
+        """ne"""
+        return not self.__eq__(other)
+
+    def __le__(self, other) -> bool:
+        """le"""
+        return self.__lt__(other) or self.__eq__(other)
+
+    def __gt__(self, other) -> bool:
+        """gt"""
+        return not self.__le__(other)
+
+    def __ge__(self, other) -> bool:
+        """ge"""
+        return not self.__lt__(other)
