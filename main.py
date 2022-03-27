2863a44c1a945f760d3aeac6847f9204250fe656
yamap55 <1785817+yamap55@users.noreply.github.com>
2022-03-27 13:39:40 +0900

    [fixed] main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 """main"""
 from logging import config
 
-from blackjack.huga import Huga
-
 config.fileConfig("logging.conf", disable_existing_loggers=False)
 
 if __name__ == "__main__":
-    Huga().piyo()
+    # TODO:
+    pass
