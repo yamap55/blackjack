3adfd0d1cf5aa3041bafe7d48b9dd174e72075e0
yamap55 <1785817+yamap55@users.noreply.github.com>
2022-03-29 23:33:41 +0900

    [add] unittest

--- tests/blackjack/test_card.py
+++ tests/blackjack/test_card.py
@@ -42,5 +42,78 @@ class TestCard:
 
             assert card1 != card2
 
+    class TestLT:
+        def test_suit(self):
+            card1 = Card(Suit.SPADE, 1)
+            card2 = Card(Suit.HEART, 1)
 
-# TODO: add unittest
+            assert card1 < card2
+
+        def test_number(self):
+            card1 = Card(Suit.SPADE, 1)
+            card2 = Card(Suit.SPADE, 2)
+
+            assert card1 < card2
+
+        def test_equal(self):
+            card1 = Card(Suit.SPADE, 1)
+            card2 = Card(Suit.SPADE, 1)
+
+            assert not card1 < card2
+
+    class TestGT:
+        def test_suit(self):
+            card1 = Card(Suit.HEART, 1)
+            card2 = Card(Suit.SPADE, 1)
+
+            assert card1 > card2
+
+        def test_number(self):
+            card1 = Card(Suit.SPADE, 2)
+            card2 = Card(Suit.SPADE, 1)
+
+            assert card1 > card2
+
+        def test_equal(self):
+            card1 = Card(Suit.SPADE, 1)
+            card2 = Card(Suit.SPADE, 1)
+
+            assert not card1 > card2
+
+    class TestLE:
+        def test_suit(self):
+            card1 = Card(Suit.SPADE, 1)
+            card2 = Card(Suit.HEART, 1)
+
+            assert card1 <= card2
+
+        def test_number(self):
+            card1 = Card(Suit.SPADE, 1)
+            card2 = Card(Suit.SPADE, 2)
+
+            assert card1 <= card2
+
+        def test_equal(self):
+            card1 = Card(Suit.SPADE, 1)
+            card2 = Card(Suit.SPADE, 1)
+
+            assert card1 <= card2
+
+    class TestGE:
+        def test_suit(self):
+            card1 = Card(Suit.HEART, 1)
+            card2 = Card(Suit.SPADE, 1)
+
+            assert card1 >= card2
+
+        def test_number(self):
+            card1 = Card(Suit.SPADE, 2)
+            card2 = Card(Suit.SPADE, 1)
+
+            assert card1 >= card2
+
+        def test_equal(self):
+            card1 = Card(Suit.SPADE, 1)
+            card2 = Card(Suit.SPADE, 1)
+
+            assert card1 >= card2
