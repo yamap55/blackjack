72490030885b34641a64ddaff83e9c1867616171
yamap55 <1785817+yamap55@users.noreply.github.com>
2022-04-02 23:19:24 +0900

    [refactor] cards -> hands

--- blackjack/player.py
+++ blackjack/player.py
@@ -8,15 +8,15 @@ from blackjack.card import Card
 class Player:
     """player"""
 
-    def __init__(self, cards: Optional[list[Card]] = None) -> None:
+    def __init__(self, hands: Optional[list[Card]] = None) -> None:
         """init"""
-        self.cards = cards or []
+        self.hands = hands or []
 
     @property
     def total(self) -> int:
         """total"""
-        return sum(card.number for card in self.cards)
+        return sum(card.number for card in self.hands)
 
     def append_card(self, card: Card) -> None:
         """append card"""
-        self.cards.append(card)
+        self.hands.append(card)
